509f2b097d0ee50140c8fc6ef585d20b0b7c758c
perepechaV6 <dima.cherkasov1234@gmail.com>
2024-11-16 11:19:06 +0300

    HomeWork1 "Базовые структуры данных"

---
+++ main.py
@@ -0,0 +1,17 @@
+#1st program
+result = (9 ** 0.5) * 5
+print(result)
+
+#2nd program
+result = (9.99 > 9.98) and (1000 != 1000.1)
+print(result)
+
+#3rd program
+result = (2 * 2 + 2 == 2 * (2 + 2))
+print(result)
+
+#4th program
+print(float('123.456'))
+print(float('123.456') * 10)
+print(int(float('123.456') * 10))
+print(int(float('123.456') * 10) % 10)
