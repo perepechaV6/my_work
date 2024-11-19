5669337b242085bc03b6f40eb2b3fefa18656d37
perepechaV6 <dima.cherkasov1234@gmail.com>
2024-11-19 15:53:32 +0300

    module_1_3.py

---
+++ module_1_3.py.py
@@ -0,0 +1,11 @@
+from operator import truediv
+
+name = 'Dima'
+print(name, (type))
+age = 24
+print(age, (type))
+new_age = 1
+print(age + new_age, (type))
+name = 'is_student'
+is_student = True
+print(name, is_student)
