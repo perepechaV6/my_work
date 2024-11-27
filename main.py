26432877d37fbbdd3481ea2f9805054403dd454a
perepechaV6 <dima.cherkasov1234@gmail.com>
2024-11-27 14:04:18 +0300

    module_1_6.py

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-#1st program
-result = (9 ** 0.5) * 5
-print(result)
-
-#2nd program
-result = (9.99 > 9.98) and (1000 != 1000.1)
-print(result)
-
-#3rd program
-result = (2 * 2 + 2 == 2 * (2 + 2))
-print(result)
-
-#4th program
-print(float('123.456'))
-print(float('123.456') * 10)
-print(int(float('123.456') * 10))
-print(int(float('123.456') * 10) % 10)
+my_dict = {'Lidiya': 2002, 'ilya': 1999, 'Liza': 2004}
+print(f"Dict: {my_dict}")
+existing_key = 'Liza'
+print(f"Existing value: {my_dict.get(existing_key)}")
+non_existing_key = 'Ivan'
+print(f"Not existing value: {my_dict.get(non_existing_key)}")
+my_dict['Uliya'] = 2000
+my_dict['Nikolay'] = 1915
+key_to_delete = 'ilya'
+deleted_value = my_dict.pop(key_to_delete, None)
+print(f"Deleted value: {deleted_value}")
+print(f"Modified dictionary: {my_dict}")
+my_set = {1, 'Помидор', 43, 1, 'Помидор'}
+print(f"Set: {my_set}")
+my_set.add(13)
+my_set.add((5, 6, 1.6))
+my_set.remove(1)
+print(f"Modified set: {my_set}")
